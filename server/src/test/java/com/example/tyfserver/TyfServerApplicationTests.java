cf9c307b7238d885f211bb1f1a915581d6bdf424
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-02 18:17:09 +0900

    server initial commit

---
+++ server/src/test/java/com/example/tyfserver/TyfServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tyfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyfServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
