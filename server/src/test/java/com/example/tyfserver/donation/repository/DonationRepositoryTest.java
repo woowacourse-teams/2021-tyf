4e58275a48ff0d41fb3a880f3e90afe72bf404dc
dwl5 <ssop6403@gmail.com>
2021-07-06 01:35:31 +0900

    refactor: 불필요한 임포트 제거

--- server/src/test/java/com/example/tyfserver/donation/repository/DonationRepositoryTest.java
+++ server/src/test/java/com/example/tyfserver/donation/repository/DonationRepositoryTest.java
@@ -9,10 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.math.BigInteger;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 class DonationRepositoryTest {
 
