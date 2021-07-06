8b185b24ebc92b2b3497322ed6cd0f4a58396278
dwl5 <ssop6403@gmail.com>
2021-07-06 13:53:58 +0900

    fix : donations RequestMapping 수정

--- server/src/main/java/com/example/tyfserver/donation/controller/DonationController.java
+++ server/src/main/java/com/example/tyfserver/donation/controller/DonationController.java
@@ -7,12 +7,10 @@ import com.example.tyfserver.donation.service.DonationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/donations")
+@RestController()
+@RequestMapping("/donations")
 @RequiredArgsConstructor
 public class DonationController {
 
