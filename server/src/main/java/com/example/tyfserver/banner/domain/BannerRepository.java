bed2b547a813e8cb0a839167f9109fd1a566c507
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-06 14:17:12 +0900

    Revert "[#8] 배너 생성 및 목록 조회 API 구현 (#10)"
    This reverts commit d31ffda53c9f25e142f8dc984aca4e69c17d2901.

--- server/src/main/java/com/example/tyfserver/banner/domain/BannerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tyfserver.banner.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BannerRepository extends JpaRepository<Banner, Long> {
-    List<Banner> findAllByMemberId(Long id);
-}
--- server/src/main/java/com/example/tyfserver/member/domain/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.tyfserver.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
