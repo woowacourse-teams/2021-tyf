fab4bcf491480e8a376711c340614ab93cf24bc7
jho2301 <jho2301@gmail.com>
2021-07-09 10:44:35 +0900

    feat: NavBar, Footer min-width 적용

--- client/src/components/Footer/Footer.styles.tsx
+++ client/src/components/Footer/Footer.styles.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
+
 import PALETTE from '../../constants/palette';
+import { MIN_WIDTH } from '../../constants/style';
 
 export const StyledFooter = styled.footer`
   background-color: ${PALETTE.GRAY_100};
   padding: 1rem;
   font-size: 0.75rem;
+  min-width: ${MIN_WIDTH};
 
   p:nth-of-type(1) {
     margin-bottom: 1rem;
---
+++ client/src/constants/style.ts
@@ -0,0 +1,2 @@
+export const MIN_WIDTH = '20rem'; // NOTE: 아이폰SE width
+export const MAX_WIDTH = '26.5rem'; // NOTE:아이폰11 PRO MAX width
