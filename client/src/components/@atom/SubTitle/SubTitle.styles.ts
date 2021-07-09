613fcf19cb40c7b6f605d8b069d51a6a4689d634
Chayan <chayans999@gmail.com>
2021-07-09 18:23:47 +0900

    style: subTitle 컴포넌트 텍스트 색상 변경

--- client/src/components/@atom/SubTitle/SubTitle.styles.ts
+++ client/src/components/@atom/SubTitle/SubTitle.styles.ts
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
-import PALETTE from '../../../constants/palette';
 
 export const StyledSubTitle = styled.h3`
   font-size: 1.5rem;
-  color: ${PALETTE.GRAY_500};
   font-weight: 600;
   text-align: center;
 `;
