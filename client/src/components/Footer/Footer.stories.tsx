e98bd520067a25dcc0a15f4c09254a7fb75b1eaa
jho2301 <jho2301@gmail.com>
2021-07-09 10:32:31 +0900

    feat: Footer 컴포넌트 구현

---
+++ client/src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, Story } from '@storybook/react';
+import Footer from './Footer';
+
+export default {
+  title: 'components/Footer',
+  component: Footer,
+} as Meta;
+
+const Template: Story = (args) => <Footer {...args} />;
+
+export const Default = Template.bind({});
