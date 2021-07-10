c863af18d7dfe2538e13edacc8ef7067cf492e85
Chayan <hchayan196@gmail.com>
2021-07-10 13:26:28 +0900

    feat: StatisticsPage 컴포넌트 구현

---
+++ client/src/pages/Statistics/StatisticsPage.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Container from '../../components/@atom/Container/Container';
+import SubTitle from '../../components/@atom/SubTitle/SubTitle';
+import Title from '../../components/@atom/Title/Title';
+
+export const StatisticsContainer = styled(Container)`
+  height: 100vh;
+`;
+
+export const InfoTitle = styled(SubTitle)`
+  margin-bottom: 6rem;
+`;
+
+export const MoneyInfo = styled(Title)`
+  margin-bottom: 4rem;
+
+  span {
+    font-size: 1rem;
+  }
+`;
