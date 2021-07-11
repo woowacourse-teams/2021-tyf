373a1932782d9200437da8c0d1b00f93965a715a
Chayan <hchayan196@gmail.com>
2021-07-11 12:04:05 +0900

    refactor: 컴포넌트 import 순서 및 타입 누락 해결

--- client/src/pages/Register/RegisterSuccess/RegisterSuccessPage.tsx
+++ client/src/pages/Register/RegisterSuccess/RegisterSuccessPage.tsx
@@ -1,7 +1,7 @@
 import { FC, HTMLAttributes } from 'react';
+
 import Anchor from '../../../components/@atom/Anchor/Anchor';
 import Template from '../../../components/@atom/Template/Template';
-
 import {
   MyPageOutlineButton,
   RegisterSuccessContainer,
