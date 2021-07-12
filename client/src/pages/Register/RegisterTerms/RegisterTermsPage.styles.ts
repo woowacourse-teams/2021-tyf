894be6f47679114d6fab092502f4d8e43af3ca43
Chayan <chayans999@gmail.com>
2021-07-12 15:22:08 +0900

    feat: 회원가입 약관 페이지 구조 및 스타일 개선

--- client/src/pages/Register/RegisterTerms/RegisterTermsPage.styles.ts
+++ client/src/pages/Register/RegisterTerms/RegisterTermsPage.styles.ts
@@ -1,35 +1,47 @@
 import styled from 'styled-components';
 import Checkbox from '../../../components/@atom/Checkbox/Checkbox';
 import Container from '../../../components/@atom/Container/Container';
+import Template from '../../../components/@atom/Template/Template';
 import Title from '../../../components/@atom/Title/Title';
 
-export const RegisterTermsContainer = styled(Container)`
-  min-height: 100vh;
+// TODO: 아래 css 모듈화
+export const StyledTemplate = styled(Template)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const TermsContainer = styled(Container)`
   margin-bottom: 4rem;
   align-items: flex-start;
+
+  /* padding-bottom: 8rem; */
 `;
 
 export const RegisterTermsTitle = styled(Title)`
   margin-bottom: 4rem;
+  width: 100%;
+  text-align: left;
+  line-height: 2.5rem;
+  padding: 0 1rem;
 `;
 
 export const TermLabel = styled.label`
-  padding: 0.5rem 0 0.5rem 1.5rem;
+  padding: 0.5rem;
   cursor: pointer;
+  display: flex;
+  align-items: center;
 `;
 
 export const TermCheckbox = styled(Checkbox)`
-  left: -1.5rem;
-  top: -0.75rem;
-  cursor: pointer;
+  margin-right: 1rem;
 `;
 
 export const Divider = styled.hr`
   width: 100%;
   border: 1px solid ${({ theme }) => theme.color.border};
+  margin: 1rem 0;
 `;
 
 export const TermLink = styled.a`
--- client/src/pages/Register/RegisterTerms/RegisterTermsPage.tsx
+++ client/src/pages/Register/RegisterTerms/RegisterTermsPage.tsx
@@ -2,11 +2,10 @@ import { FC, HTMLAttributes } from 'react';
 
 import Button from '../../../components/@atom/Button/Button';
 import Container from '../../../components/@atom/Container/Container';
-import Template from '../../../components/@atom/Template/Template';
 import {
   Divider,
-  RegisterTermsContainer,
   RegisterTermsTitle,
+  StyledTemplate,
   TermCheckbox,
   TermLabel,
   TermLink,
@@ -15,35 +14,37 @@ import {
 
 const RegisterTermsPage: FC<HTMLAttributes<HTMLElement>> = () => {
   return (
-    <Template>
-      <RegisterTermsContainer>
-        <RegisterTermsTitle>서비스 약관에 동의해 주세요.</RegisterTermsTitle>
-        <TermsContainer>
-          <TermLabel>
-            <TermCheckbox></TermCheckbox>
-            전체 동의
-          </TermLabel>
-          <Divider />
-          <TermLabel>
-            <TermCheckbox></TermCheckbox>
-            <TermLink href="" target="_blank">
-              서비스 약관
-            </TermLink>
-            에 동의 (필수)
-          </TermLabel>
-          <TermLabel>
-            <TermCheckbox></TermCheckbox>
-            <TermLink href="" target="_blank">
-              개인정보 수집 및 이용
-            </TermLink>
-            에 동의 (필수)
-          </TermLabel>
-        </TermsContainer>
-        <Container>
-          <Button disabled>계속하기</Button>
-        </Container>
-      </RegisterTermsContainer>
-    </Template>
+    <StyledTemplate>
+      <RegisterTermsTitle>
+        서비스
+        <br /> 약관에
+        <br /> 동의해 주세요.
+      </RegisterTermsTitle>
+      <TermsContainer>
+        <TermLabel>
+          <TermCheckbox></TermCheckbox>
+          전체 동의
+        </TermLabel>
+        <Divider />
+        <TermLabel>
+          <TermCheckbox></TermCheckbox>
+          <TermLink href="" target="_blank">
+            서비스 약관
+          </TermLink>
+          에 동의 (필수)
+        </TermLabel>
+        <TermLabel>
+          <TermCheckbox></TermCheckbox>
+          <TermLink href="" target="_blank">
+            개인정보 수집 및 이용
+          </TermLink>
+          에 동의 (필수)
+        </TermLabel>
+      </TermsContainer>
+      <Container>
+        <Button disabled>계속하기</Button>
+      </Container>
+    </StyledTemplate>
   );
 };
 
