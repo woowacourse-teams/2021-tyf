1cf853eb80ce085b34f723d122c3f83c91dc89b4
Chayan <hchayan196@gmail.com>
2021-07-15 03:41:30 +0900

    refactor: oauth 상수화

--- client/src/components/Register/RegisterAuthForm/RegisterAuthForm.tsx
+++ client/src/components/Register/RegisterAuthForm/RegisterAuthForm.tsx
@@ -1,6 +1,7 @@
 import { VFC } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import Anchor from '../../@atom/Anchor/Anchor';
 import GoogleLogo from '../../../assets/icons/google.svg';
 import NaverLogo from '../../../assets/icons/naver.svg';
 import KakaoLogo from '../../../assets/icons/kakao.svg';
@@ -12,25 +13,25 @@ import {
   KakaoButton,
   NaverButton,
 } from './RegisterAuthForm.styles';
-import Anchor from '../../@atom/Anchor/Anchor';
-
 import { OAUTH } from '../../../constants/constant';
 
 const RegisterAuthForm: VFC = () => {
   const history = useHistory();
+
   const openOAuthPage = (name: string) => {
     switch (name) {
-      case 'goggle':
-        window.open(OAUTH.GOOGLE);
+      case OAUTH.GOOGLE.NAME:
+        window.open(OAUTH.GOOGLE.URL, '_target');
         break;
 
-      case 'naver':
+      case OAUTH.NAVER.NAME:
+        // (다음 페이지로 넘어가기 위한 임시 코드로 대체)
+        // window.open(OAUTH.NAVER, '_target');
         history.push('/register/url');
         break;
 
-      case 'kakao':
-        // TODO: oauth 회원가입 절차 진행
-        window.open(OAUTH.KAKAO);
+      case OAUTH.KAKAO.NAME:
+        window.open(OAUTH.KAKAO.URL, '_target');
         break;
 
       default:
@@ -41,13 +42,25 @@ const RegisterAuthForm: VFC = () => {
     <>
       <RegisterTitle>회원가입</RegisterTitle>
       <RegisterButtonContainer>
-        <GoogleButton src={GoogleLogo} alt="google_logo" onClick={() => openOAuthPage('goggle')}>
+        <GoogleButton
+          src={GoogleLogo}
+          alt="google_logo"
+          onClick={() => openOAuthPage(OAUTH.GOOGLE.NAME)}
+        >
           구글 회원가입
         </GoogleButton>
-        <NaverButton src={NaverLogo} alt="naver_logo" onClick={() => openOAuthPage('naver')}>
+        <NaverButton
+          src={NaverLogo}
+          alt="naver_logo"
+          onClick={() => openOAuthPage(OAUTH.NAVER.NAME)}
+        >
           네이버 회원가입
         </NaverButton>
-        <KakaoButton src={KakaoLogo} alt="kakao_logo" onClick={() => openOAuthPage('kakao')}>
+        <KakaoButton
+          src={KakaoLogo}
+          alt="kakao_logo"
+          onClick={() => openOAuthPage(OAUTH.KAKAO.NAME)}
+        >
           카카오 회원가입
         </KakaoButton>
       </RegisterButtonContainer>
