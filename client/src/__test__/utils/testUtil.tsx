95f06883ea7802fa6da939d9b42987c6af5abea4
jho2301 <jho2301@gmail.com>
2021-07-15 15:04:18 +0900

    test: 창작자 리스트를 조회한다.

--- client/src/__test__/utils/testUtil.tsx
+++ client/src/__test__/utils/testUtil.tsx
@@ -1,11 +1,15 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
+
 import { theme } from '../../theme';
 
 export const myRender = (children: React.ReactNode) =>
   render(
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>{children}</BrowserRouter>
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </ThemeProvider>
+    </RecoilRoot>
   );
