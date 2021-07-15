441a64ef365b9db1d9fb935051f22e8562fca5ec
dwl5 <ssop6403@gmail.com>
2021-07-15 19:15:04 +0900

    feat: application-test 추가

--- server/src/test/java/com/example/tyfserver/TyfServerApplicationTests.java
+++ server/src/test/java/com/example/tyfserver/TyfServerApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.tyfserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TyfServerApplicationTests {
 
