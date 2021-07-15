e4e8dfd122e3c3c71fc23bba39428c290fd3eb91
dwl5 <ssop6403@gmail.com>
2021-07-15 12:43:56 +0900

    feat: jasypt secret ket 외부에서 가져오도록 수정

--- server/src/main/java/com/example/tyfserver/TyfServerApplication.java
+++ server/src/main/java/com/example/tyfserver/TyfServerApplication.java
@@ -1,10 +1,13 @@
 package com.example.tyfserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
 @EnableJpaAuditing
+@EnableEncryptableProperties
 @SpringBootApplication
 public class TyfServerApplication {
 
--- server/src/main/java/com/example/tyfserver/common/config/JasyptConfig.java
+++ server/src/main/java/com/example/tyfserver/common/config/JasyptConfig.java
@@ -5,15 +5,22 @@ import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class JasyptConfig {
+
+    private final Environment environment;
+
+    public JasyptConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor jasyptStringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(System.getProperty("tyfserver"));
+        config.setPassword(environment.getProperty("jasypt.encryptor.password"));
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
