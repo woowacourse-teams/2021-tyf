bfdfb0d29faf1f5064d714fea0beb7aa5a5dd61d
김주원 <kjw11077@naver.com>
2021-07-15 12:48:50 +0900

    refactor: @ControllerAdvice -> @RestControllerAdvice 변경

--- server/src/main/java/com/example/tyfserver/common/config/GlobalExceptionHandler.java
+++ server/src/main/java/com/example/tyfserver/common/config/GlobalExceptionHandler.java
@@ -5,9 +5,10 @@ import com.example.tyfserver.common.exception.BaseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class ControllerAdviceHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> errorHandling(BaseException exception) {
