37e0a441e8eecb06644707a65ef25b78090ec662
jho2301 <jho2301@gmail.com>
2021-07-16 14:54:53 +0900

    feat: web share API 없는 경우 clipboard에 복사

--- client/src/service/share.ts
+++ client/src/service/share.ts
@@ -1,9 +1,11 @@
 export const donationUrlShare = (creatorName: string, pageName: string) => {
-  if (!window.navigator.share) return null;
+  const url = `${window.location.origin}/donation/${pageName}`;
+  const title = `${creatorName}님에게 도네이션하기 | Thank You For ___`;
+  const text = `${creatorName}님에게 감사를 표시 해보세요!`;
 
-  return window.navigator.share({
-    title: `${creatorName}님에게 도네이션하기 | Thank You For ___`,
-    text: `${creatorName}님에게 감사를 표시 해보세요!`,
-    url: `${window.location.origin}/donation/${pageName}`,
-  });
+  if (window.navigator.share) {
+    return window.navigator.share({ title, text, url });
+  }
+
+  return navigator.clipboard.writeText(url);
 };
