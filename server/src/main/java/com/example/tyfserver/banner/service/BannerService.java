506b54704a58a91654ed922af669d7530d7d8a12
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-16 22:52:21 +0900

    feat: BannerServiceTest 작성

--- server/src/main/java/com/example/tyfserver/banner/service/BannerService.java
+++ server/src/main/java/com/example/tyfserver/banner/service/BannerService.java
@@ -2,8 +2,8 @@ package com.example.tyfserver.banner.service;
 
 import com.example.tyfserver.auth.dto.LoginMember;
 import com.example.tyfserver.banner.domain.Banner;
-import com.example.tyfserver.banner.repository.BannerRepository;
 import com.example.tyfserver.banner.dto.BannerResponse;
+import com.example.tyfserver.banner.repository.BannerRepository;
 import com.example.tyfserver.member.domain.Member;
 import com.example.tyfserver.member.exception.MemberNotFoundException;
 import com.example.tyfserver.member.repository.MemberRepository;
@@ -25,13 +25,11 @@ public class BannerService {
     public Long createBanner(LoginMember loginMember, String imageUrl) {
         Member member = memberRepository.findById(loginMember.getId())
                 .orElseThrow(MemberNotFoundException::new);
-        Banner banner = bannerRepository.save(new Banner(member, imageUrl));
-        return banner.getId();
+        return bannerRepository.save(new Banner(member, imageUrl)).getId();
     }
 
     public List<BannerResponse> getBanners(LoginMember loginMember) {
         List<Banner> banners = bannerRepository.findAllByMemberId(loginMember.getId());
-        //todo: 이 부분 비어있는 경우 고려해봐야 할 듯
         return banners.stream()
                 .map(BannerResponse::new)
                 .collect(Collectors.toList());
