ed70ebad2e5f0061a178ef084f52d33ed8704e96
jho2301 <jho2301@gmail.com>
2021-07-16 10:54:34 +0900

    feat: 로고버튼을 누르면 홈화면으로 이동한다

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,14 @@
+import { useHistory } from 'react-router-dom';
+import Anchor from '../@atom/Anchor/Anchor';
 import { StyledNavBar, StyledLogo, HamburgerButton, LoginButton } from './NavBar.styles';
 
 const NavBar = () => {
+  const history = useHistory();
+
   return (
     <StyledNavBar>
       <HamburgerButton />
-      <StyledLogo />
+      <StyledLogo onClick={() => history.push('/')} />
       <LoginButton to="/login">로그인</LoginButton>
     </StyledNavBar>
   );
