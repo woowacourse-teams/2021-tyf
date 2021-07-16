483bb77eee7bba70c8652beba20663fb8789f019
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-17 00:02:41 +0900

    refactor: @EnableJpaAuditing 선언 위치 변경
    @WebMvcTest 시에 jpa 생성과 관련된 기능이 들어오지 않는데 main 함수가 속한 클래스 위에 있으면 이 떄문에 테스트 위에 추가적인 어노테이션이 계속해서 붙게된다. reference: https://1-7171771.tistory.com/136?category=981620

--- server/src/main/java/com/example/tyfserver/TyfServerApplication.java
+++ server/src/main/java/com/example/tyfserver/TyfServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TyfServerApplication {
 
---
+++ server/src/main/java/com/example/tyfserver/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.tyfserver.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
