8d0efc318d8f51ee565c06973cd5bf0d1147f1b6
Chayan <chayans999@gmail.com>
2021-07-17 10:59:03 +0900

    fix: 흐름 경로 라우터 수정

--- client/src/components/Register/RegisterAuthForm/RegisterAuthForm.tsx
+++ client/src/components/Register/RegisterAuthForm/RegisterAuthForm.tsx
@@ -26,7 +26,7 @@ const RegisterAuthForm: VFC = () => {
         {/* <NaverBarButton onClick={() => routeToOAuthPage('NAVER', 'REGISTER')}>
           네이버 회원가입
         </NaverBarButton> */}
-        <NaverBarButton onClick={() => history.push('/register/name')}>
+        <NaverBarButton onClick={() => history.push('/register/url')}>
           네이버 회원가입
         </NaverBarButton>
         <KakaoBarButton onClick={() => routeToOAuthPage('KAKAO', 'REGISTER')}>
