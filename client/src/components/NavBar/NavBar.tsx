b7f1f33fbbc15dcd4c5e459f61f53d0270ea2d9b
jho2301 <jho2301@gmail.com>
2021-07-18 18:38:17 +0900

    fix: 후원 금액 입력 정상화
    인풋을 빈칸으로 만들고 금액추가 버튼을 누르면 금액이 추가되지 않는 현상

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,18 +1,25 @@
 import { useHistory } from 'react-router-dom';
+import useLoginUserInfo from '../../service/hooks/useLoginUserInfo';
 import useModal from '../../utils/useModal';
+import Anchor from '../@atom/Anchor/Anchor';
 import Menu from '../Menu/Menu';
 import { StyledNavBar, StyledLogo, HamburgerButton, LoginButton } from './NavBar.styles';
 
 const NavBar = () => {
   const history = useHistory();
   const { isOpen, toggleModal, closeModal } = useModal();
+  const { userInfo } = useLoginUserInfo();
 
   return (
     <>
       <StyledNavBar>
         <HamburgerButton onClick={() => toggleModal()} />
         <StyledLogo onClick={() => history.push('/')} />
-        <LoginButton to="/login">로그인</LoginButton>
+        {userInfo ? (
+          <Anchor to={`/creator/${userInfo.pageName}`}>{userInfo.nickname}</Anchor>
+        ) : (
+          <LoginButton to="/login">로그인</LoginButton>
+        )}
       </StyledNavBar>
       {isOpen && <Menu onClose={closeModal} />}
     </>
