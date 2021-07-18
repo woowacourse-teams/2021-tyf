cebf65036e0b5356dc70e6f18b7956d0d7006667
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-18 17:05:29 +0900

    refactor: enum toUpperCase 추가

--- server/src/main/java/com/example/tyfserver/TyfServerApplication.java
+++ server/src/main/java/com/example/tyfserver/TyfServerApplication.java
@@ -2,8 +2,6 @@ package com.example.tyfserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 
 @SpringBootApplication
 public class TyfServerApplication {
