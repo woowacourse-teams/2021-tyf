4f9108a127296ac3ff75ac651eb0bccca8a73ed3
jho2301 <jho2301@gmail.com>
2021-07-18 10:27:03 +0900

    oauth redirect_uri 설정

--- client/src/constants/oauth.ts
+++ client/src/constants/oauth.ts
@@ -1,14 +1,16 @@
 export const AUTH_CODE = 'code';
 
+const origin = window.location.origin;
+
 export const OAUTH = {
   KAKAO: {
-    URL: 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=d187d243372db29c85e85eef81d4723a&redirect_uri=http://localhost:9000/register/url',
+    URL: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=ff77c9c32b464ffb0d98f3d04f7a7a48&redirect_uri=${origin}`,
   },
   NAVER: {
-    URL: '',
+    URL: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=ZJkZDegYIcTIZu8NP5qI&state=STATE_STRING&redirect_uri=${origin}`,
   },
   GOOGLE: {
-    URL: 'https://accounts.google.com/o/oauth2/v2/auth?client_id=153785509866-72pebue5t5qqcpci2d1bncrh497ootcc.apps.googleusercontent.com&redirect_uri=http://localhost:9000/register/url&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile email&access_type=offline',
+    URL: `https://accounts.google.com/o/oauth2/v2/auth?client_id=153785509866-72pebue5t5qqcpci2d1bncrh497ootcc.apps.googleusercontent.com&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile%20email&access_type=offline&redirect_uri=${origin}`,
   },
 } as const;
 
