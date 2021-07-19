7c9bdb61fbb269b7fef153d3ce1c98336dcff8ab
jho2301 <jho2301@gmail.com>
2021-07-19 10:12:27 +0900

    chore: favicon 세팅

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -42,6 +42,7 @@ const config: WebpackConfig = {
     new HtmlWebpackPlugin({
       template: path.resolve('public', 'index.html'),
       base: '/',
+      favicon: path.resolve('public', 'favicon.ico'),
     }),
     new webpack.ProvidePlugin({
       process: 'process/browser',
