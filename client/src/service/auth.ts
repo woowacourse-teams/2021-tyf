84a23d8d7678f26b067b4dcf0ee2d05f8db51de1
jho2301 <jho2301@gmail.com>
2021-07-19 10:48:34 +0900

    fix: 로그인버튼 클릭시 새창이 뜨는 현상

--- client/src/service/auth.ts
+++ client/src/service/auth.ts
@@ -4,5 +4,5 @@ import { OAuthProvider } from '../types';
 type RedirectPathType = keyof typeof REDIRECT_PATH;
 
 export const routeToOAuthPage = (oauthName: OAuthProvider, redirectPath: RedirectPathType) => {
-  window.open(OAUTH[oauthName].URL + REDIRECT_PATH[redirectPath] + oauthName, '_target');
+  window.open(OAUTH[oauthName].URL + REDIRECT_PATH[redirectPath] + oauthName, '_self');
 };
