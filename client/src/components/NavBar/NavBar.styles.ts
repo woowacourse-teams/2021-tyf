518b872de6ccdf15a2507d15bdab4f47782b256a
jho2301 <jho2301@gmail.com>
2021-07-23 12:11:35 +0900

    feat: 햄버거 버튼 제거
    - 햄버거버튼 기능 유저닉네임 버튼으로 통합

--- client/src/components/NavBar/NavBar.styles.ts
+++ client/src/components/NavBar/NavBar.styles.ts
@@ -6,22 +6,16 @@ import hamburgerImg from '../../assets/icons/hamburger.svg';
 import logoImg from '../../assets/images/logo.svg';
 import Anchor from '../@atom/Anchor/Anchor';
 import { SIZE } from '../../constants/device';
+import TextButton from '../@atom/TextButton/TextButton.styles';
 
 export const StyledNavBar = styled(Container)`
   height: 3.5rem;
   border-bottom: 1px solid ${({ theme }) => theme.color.border};
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: flex-end;
   padding: 0 1rem;
   min-width: ${SIZE.MOBILE_MIN};
   position: relative;
-`;
-
-export const HamburgerButton = styled(IconButton).attrs({
-  src: hamburgerImg,
-})`
-  width: 1.5rem;
-  height: 1.5rem;
 `;
 
 export const StyledLogo = styled(IconButton).attrs({
@@ -37,3 +31,5 @@ export const LoginButton = styled(Anchor)`
   font-size: 0.875rem;
   font-weight: 400;
 `;
+
+export const StyledTextButton = styled(TextButton)``;
