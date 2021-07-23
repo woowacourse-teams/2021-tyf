66324149c34f59b972f9219c4ac9402ba4f091bb
jho2301 <jho2301@gmail.com>
2021-07-23 17:06:53 +0900

    fix: 로그인 유지하기 동작하지 않는 현상
    - 로그인시 리다이렉션으로 페이지내 전역상태가 초기화되는 이슈가 있었음
    - 로그인 유지여부를 전역상태로 관리해서 초기화가 됨
    - 로그인 유지여부 자체를 로컬스토리지에 저장

--- client/src/service/hooks/useAccessToken.ts
+++ client/src/service/hooks/useAccessToken.ts
@@ -1,20 +1,22 @@
 import { useRecoilState } from 'recoil';
 import { STORAGE_KEY } from '../../constants/storage';
 import { StorageType } from '../../types';
-import { setLocalStorageItem, setSessionStorageItem } from '../../utils/storage';
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+  setSessionStorageItem,
+} from '../../utils/storage';
 import { accessTokenState } from '../state/login';
 
 const useAccessToken = () => {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 
-  const storeAccessToken = (accessToken: string, storageType: StorageType) => {
-    if (storageType === 'LOCAL') {
-      setLocalStorageItem(STORAGE_KEY.ACCESS_TOKEN, accessToken);
-    }
+  const storeAccessToken = (accessToken: string) => {
+    const isLoginPersist = getLocalStorageItem(STORAGE_KEY.IS_LOGIN_PERSIST);
 
-    if (storageType === 'SESSION') {
-      setSessionStorageItem(STORAGE_KEY.ACCESS_TOKEN, accessToken);
-    }
+    isLoginPersist
+      ? setLocalStorageItem(STORAGE_KEY.ACCESS_TOKEN, accessToken)
+      : setSessionStorageItem(STORAGE_KEY.ACCESS_TOKEN, accessToken);
 
     setAccessToken(accessToken);
   };
--- client/src/service/hooks/useRegister.ts
+++ client/src/service/hooks/useRegister.ts
@@ -44,7 +44,7 @@ const useRegister = () => {
       }
       const { token } = await requestRegister(user);
 
-      storeAccessToken(token, 'SESSION');
+      storeAccessToken(token);
 
       history.push('/register/success');
     } catch (error) {
