e130607c15725e7151e3c67f5bc360cbe919a266
Chayan <hchayan196@gmail.com>
2021-07-23 15:17:53 +0900

    style: 후원 통계 페이지 데스크탑 마크업 추가

--- client/src/pages/Statistics/StatisticsPage.styles.ts
+++ client/src/pages/Statistics/StatisticsPage.styles.ts
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 import SubTitle from '../../components/@atom/SubTitle/SubTitle';
 import Template from '../../components/@atom/Template/Template';
 import Title from '../../components/@atom/Title/Title';
+import { SIZE } from '../../constants/device';
 
 export const StyledTemplate = styled(Template)`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  max-width: ${SIZE.MOBILE_MAX};
 `;
 
 export const InfoTitle = styled(SubTitle)`
