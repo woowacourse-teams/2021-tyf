604b1cc930915e202b252a00e98af242a7a76a91
dwl5 <ssop6403@gmail.com>
2021-07-26 20:30:44 +0900

    refactor: CloudFrontUrlGenerator ActiveProfile추가

--- server/src/test/java/com/example/tyfserver/common/util/CloudFrontUrlGeneratorTest.java
+++ server/src/test/java/com/example/tyfserver/common/util/CloudFrontUrlGeneratorTest.java
@@ -3,10 +3,12 @@ package com.example.tyfserver.common.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CloudFrontUrlGeneratorTest {
 
     @Test
