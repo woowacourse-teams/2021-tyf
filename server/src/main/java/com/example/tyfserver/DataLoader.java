23f558eb545d1f5296d9e3a7baff8ca4d1e38491
dwl5 <ssop6403@gmail.com>
2021-07-26 20:20:55 +0900

    refactor: DataLoader dev만 적용되도록 수정

--- server/src/main/java/com/example/tyfserver/DataLoader.java
+++ server/src/main/java/com/example/tyfserver/DataLoader.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!dev")
+@Profile("dev")
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
