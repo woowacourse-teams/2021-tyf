f88b051ff79b137456467d582d3c8ee1e2d12cda
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-28 17:50:47 +0900

    fix: 젠킨스 테스트

--- server/src/main/java/com/example/tyfserver/TyfServerApplication.java
+++ server/src/main/java/com/example/tyfserver/TyfServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TyfServerApplication {
 
     public static void main(String[] args) {
+        System.out.println("(");
         SpringApplication.run(TyfServerApplication.class, args);
     }
 
