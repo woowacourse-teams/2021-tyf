bc66135392b8a1e288d10222f47aa72b99f8429f
jho2301 <jho2301@gmail.com>
2021-07-28 21:32:56 +0900

    fix: 로그인 후 후원 총액이 변경되지 않는 현상

--- client/src/service/hooks/useStatistics.ts
+++ client/src/service/hooks/useStatistics.ts
@@ -1,9 +1,18 @@
-import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { requestIdState } from '../state/request';
 
 import { userStatisticsQuery } from '../state/statistics';
+import useAccessToken from './useAccessToken';
 
 const useStatistics = () => {
+  const { accessToken } = useAccessToken();
   const { point: totalAmount } = useRecoilValue(userStatisticsQuery);
+  const [requestId, setRequestId] = useRecoilState(requestIdState(accessToken));
+
+  useEffect(() => {
+    setRequestId(requestId + 1);
+  }, []);
 
   return { totalAmount };
 };
--- client/src/service/state/statistics.ts
+++ client/src/service/state/statistics.ts
@@ -2,11 +2,14 @@ import { selector } from 'recoil';
 import { Statistics } from '../../types';
 import { requestUserStatistics } from '../request/statistics';
 import { accessTokenState } from './login';
+import { requestIdState } from './request';
 
 export const userStatisticsQuery = selector<Statistics>({
   key: 'userStatisticsQuery',
   get: ({ get }) => {
     const accessToken = get(accessTokenState);
+
+    get(requestIdState(accessToken));
 
     return requestUserStatistics(accessToken);
   },
