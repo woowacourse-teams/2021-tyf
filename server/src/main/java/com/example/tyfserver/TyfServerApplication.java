d943406014dc48477e6563b13aa449169e0abcfe
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-28 18:54:02 +0900

    Update TyfServerApplication.java (#192)

--- server/src/main/java/com/example/tyfserver/TyfServerApplication.java
+++ server/src/main/java/com/example/tyfserver/TyfServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TyfServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("changed webhook trigger");
+        System.out.println("jenkins test");
         SpringApplication.run(TyfServerApplication.class, args);
     }
 
