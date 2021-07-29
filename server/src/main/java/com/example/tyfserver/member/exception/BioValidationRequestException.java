3c8cbb05460b662434edee89471ea59336c3528f
rok93 <goodboy302@naver.com>
2021-07-29 20:12:39 +0900

    refactor: 예외 error message 수정

--- server/src/main/java/com/example/tyfserver/member/exception/BioValidationRequestException.java
+++ server/src/main/java/com/example/tyfserver/member/exception/BioValidationRequestException.java
@@ -5,7 +5,7 @@ import com.example.tyfserver.common.exception.BaseException;
 public class BioValidationRequestException extends BaseException {
 
     public static final String ERROR_CODE = "member-006";
-    private static final String MESSAGE = "Bio 유효 검사 시에 넘기는 Request의 값이 유효한 값이 아닙니다.";
+    private static final String MESSAGE = "Bio 형식이 올바르지 않습니다.";
 
     public BioValidationRequestException() {
         super(ERROR_CODE, MESSAGE);
