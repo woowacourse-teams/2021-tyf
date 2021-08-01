6ab3ab1df2ae8fca5ffde3cb0fd0d6d4228f83d4
jho2301 <jho2301@gmail.com>
2021-08-01 23:46:20 +0900

    refactor: useDonation donate함수 가독성 개선

--- client/src/service/hooks/useDonation.ts
+++ client/src/service/hooks/useDonation.ts
@@ -18,41 +18,42 @@ const useDonation = (creatorId: CreatorId) => {
     const { merchantUid } = await requestPayment({ amount, email, pageName });
     const { IMP } = window;
 
-    IMP.init('imp61348931'); // TODO: 가맹점 식별번호 입력
+    const accountId = 'imp61348931';
 
-    // TODO : 후원자 정보 입력 form 페이지 추가하기
-    IMP.request_pay(
-      {
-        pg: 'kakaopay',
-        pay_method: 'card',
-        merchant_uid: merchantUid,
-        name: pageName,
-        amount,
-        buyer_email: email,
-        // buyer_name: '홍길동',
-        // buyer_tel: '010-4242-4242',
-        // buyer_addr: '서울특별시 강남구 신사동',
-        // buyer_postcode: '01181',
-      },
-      async (response: IamportResponse) => {
-        if (response.success) {
-          try {
-            const donationResult = await requestPaymentComplete(response);
+    IMP.init(accountId);
 
-            setDonation(donationResult);
+    const IMPRequestPayOption = {
+      pg: 'kakaopay',
+      pay_method: 'card',
+      merchant_uid: merchantUid,
+      name: pageName,
+      amount,
+      buyer_email: email,
+      // buyer_name: '홍길동',
+      // buyer_tel: '010-4242-4242',
+      // buyer_addr: '서울특별시 강남구 신사동',
+      // buyer_postcode: '01181',
+    };
 
-            alert('결제에 성공했습니다.');
-            history.push(`/donation/${pageName}/message`);
-          } catch (error) {
-            alert(`결제에 실패했습니다. 다시 시도해주세요.${error.message}`);
-            window.close();
-          }
-        } else {
-          alert(`결제에 실패했습니다. 다시 시도해주세요.\n 에러 내역: ${response.error_msg}`);
-          window.close();
-        }
+    const IMPResponseHandler = async (response: IamportResponse) => {
+      if (!response.success) {
+        alert(`결제에 실패했습니다. 다시 시도해주세요.\n 에러 내역: ${response.error_msg}`);
+        window.close();
+        return;
       }
-    );
+
+      try {
+        const donationResult = await requestPaymentComplete(response);
+        setDonation(donationResult);
+        alert('결제에 성공했습니다.');
+        history.push(`/donation/${pageName}/message`);
+      } catch (error) {
+        alert(`결제에 실패했습니다. 다시 시도해주세요. ${error.message}`);
+        window.close();
+      }
+    };
+
+    IMP.request_pay(IMPRequestPayOption, IMPResponseHandler);
   };
 
   return { donate, donation };
