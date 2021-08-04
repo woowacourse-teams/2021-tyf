db15de9eb14cb42823c54bc16d17aa6020daf6dc
Chayan <hchayan196@gmail.com>
2021-08-04 19:30:24 +0900

    feat: 후원 이메일 인증 페이지 마크업 구현

---
+++ client/src/pages/Refund/Certification/RefundCertificationPage.tsx
@@ -0,0 +1,12 @@
+import RefundCertificationForm from '../../../components/Refund/CertificationForm/RefundCertificationForm';
+import { StyledTemplate } from './RefundCertificationPage.styles';
+
+const RefundCertificationPage = () => {
+  return (
+    <StyledTemplate>
+      <RefundCertificationForm />
+    </StyledTemplate>
+  );
+};
+
+export default RefundCertificationPage;
