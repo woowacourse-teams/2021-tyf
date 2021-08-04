63c4ec23ed762dbdd159a1f528262e63dfe40d81
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-08-04 17:33:55 +0900

    refactor: Oauth2ServiceConnector로 역할 분리

--- server/src/main/java/com/example/tyfserver/auth/service/Oauth2Service.java
+++ server/src/main/java/com/example/tyfserver/auth/service/Oauth2Service.java
@@ -1,6 +1,5 @@
 package com.example.tyfserver.auth.service;
 
-import com.example.tyfserver.auth.domain.Oauth2;
 import com.example.tyfserver.auth.domain.Oauth2Type;
 import com.example.tyfserver.auth.dto.Oauth2Request;
 import com.example.tyfserver.auth.dto.SignUpResponse;
@@ -8,23 +7,13 @@ import com.example.tyfserver.auth.dto.TokenResponse;
 import com.example.tyfserver.auth.exception.AlreadyRegisteredException;
 import com.example.tyfserver.auth.exception.AlreadyRegisteredInSameOauth2TypeException;
 import com.example.tyfserver.auth.exception.UnregisteredMemberException;
-import com.example.tyfserver.common.util.ApiSender;
+import com.example.tyfserver.auth.util.Oauth2ServiceConnector;
 import com.example.tyfserver.member.domain.Member;
 import com.example.tyfserver.member.dto.SignUpReadyResponse;
 import com.example.tyfserver.member.dto.SignUpRequest;
 import com.example.tyfserver.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -32,9 +21,10 @@ public class Oauth2Service {
 
     private final MemberRepository memberRepository;
     private final AuthenticationService authenticationService;
+    private final Oauth2ServiceConnector oauth2ServiceConnector;
 
     public TokenResponse login(final Oauth2Request oauth2Request, final String code) {
-        final String email = getEmailFromOauth2(oauth2Request, code);
+        final String email = oauth2ServiceConnector.getEmailFromOauth2(oauth2Request, code);
 
         Member findMember = memberRepository.findByEmailAndOauth2Type(email, Oauth2Type.findOauth2Type(oauth2Request.getType()))
                 .orElseThrow(UnregisteredMemberException::new);
@@ -43,7 +33,7 @@ public class Oauth2Service {
     }
 
     public SignUpReadyResponse readySignUp(final Oauth2Request oauth2Request, final String code) {
-        final String email = getEmailFromOauth2(oauth2Request, code);
+        final String email = oauth2ServiceConnector.getEmailFromOauth2(oauth2Request, code);
 
         memberRepository.findByEmail(email)
                 .ifPresent(member -> validateRegisteredMember(oauth2Request.getType(), member));
@@ -58,72 +48,10 @@ public class Oauth2Service {
         return new SignUpResponse(authenticationService.createToken(persistMember), persistMember.getPageName());
     }
 
-    private String getEmailFromOauth2(Oauth2Request oauth2Request, String code) {
-        final String accessToken = requestAccessToken(code, oauth2Request);
-        return requestEmail(accessToken, oauth2Request);
-    }
-
-    private String requestAccessToken(String code, Oauth2Request oauth2Request) {
-        String body = ApiSender.send(
-                oauth2Request.getAccessTokenApi(),
-                HttpMethod.POST,
-                generateAccessTokenRequest(code, oauth2Request)
-        );
-
-        return extractAccessToken(body);
-    }
-
-    private String requestEmail(String accessToken, Oauth2Request oauth2Request) {
-        String body = ApiSender.send(
-                oauth2Request.getProfileApi(),
-                HttpMethod.GET,
-                generateProfileRequest(accessToken)
-        );
-
-        Oauth2 oauth2 = Oauth2Type.findOauth2(oauth2Request.getType());
-        return extractEmail(oauth2, body);
-    }
-
     private void validateRegisteredMember(String oauthType, Member member) {
         if (member.isSameOauthType(oauthType)) {
             throw new AlreadyRegisteredInSameOauth2TypeException(authenticationService.createToken(member));
         }
         throw new AlreadyRegisteredException(member.getOauth2Type().name());
-    }
-
-    private HttpEntity<MultiValueMap<String, String>> generateAccessTokenRequest(String code, Oauth2Request oauth2Request) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", oauth2Request.getClientId());
-        params.add("redirect_uri", oauth2Request.getRedirectUrl());
-        params.add("code", code);
-        params.add("client_secret", oauth2Request.getClientSecret());
-
-        return new HttpEntity<>(params, headers);
-    }
-
-    private HttpEntity<MultiValueMap<String, String>> generateProfileRequest(String accessToken) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
-
-        headers.setBearerAuth(accessToken);
-        return new HttpEntity<>(headers);
-    }
-
-    private String extractEmail(Oauth2 oauth2, String response) {
-        // todo OAuth2 에서 발생한 예외 잡기
-        final JSONObject jsonObject = new JSONObject(response);
-        return oauth2.extractEmail(jsonObject);
-    }
-
-    private String extractAccessToken(String body) {
-        // todo OAuth2 에서 발생한 예외 잡기
-        final JSONObject jsonObject = new JSONObject(body);
-        return jsonObject.getString("access_token");
     }
 }
