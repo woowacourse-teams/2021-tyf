820c3c620b299e2b915851c211b21afb83e74890
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-08-05 01:13:50 +0900

    refactor: AcceptanceTest 변경

--- server/src/test/java/com/example/tyfserver/AcceptanceTest.java
+++ server/src/test/java/com/example/tyfserver/AcceptanceTest.java
@@ -1,13 +1,30 @@
 package com.example.tyfserver;
 
+import com.example.tyfserver.auth.domain.Oauth2Type;
+import com.example.tyfserver.auth.dto.TokenResponse;
+import com.example.tyfserver.auth.service.Oauth2Service;
+import com.example.tyfserver.auth.util.JwtTokenProvider;
+import com.example.tyfserver.auth.util.Oauth2ServiceConnector;
+import com.example.tyfserver.member.domain.Member;
+import com.example.tyfserver.member.dto.SignUpReadyResponse;
+import com.example.tyfserver.member.repository.MemberRepository;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -15,6 +32,15 @@ public class AcceptanceTest {
 
     @LocalServerPort
     int port;
+
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+    @MockBean
+    private Oauth2ServiceConnector oauth2ServiceConnector;
+
+    private Member mainMember = new Member("main@gmail.com", "nickname", "pageName", Oauth2Type.GOOGLE, "profileImage");
 
     protected static RequestSpecification apiTemplate() {
         return RestAssured
@@ -31,6 +57,19 @@ public class AcceptanceTest {
 
     protected static ValidatableResponse get(String url) {
         return apiTemplate()
+                .get(url)
+                .then().log().all();
+    }
+
+    protected static RequestSpecification paramTemplate(String name, String param) {
+        return RestAssured
+                .given().log().all()
+                .param(name, param)
+                .contentType(MediaType.APPLICATION_JSON_VALUE);
+    }
+
+    protected static ValidatableResponse paramGet(String url, String name, String param) {
+        return paramTemplate(name, param)
                 .get(url)
                 .then().log().all();
     }
@@ -56,5 +95,13 @@ public class AcceptanceTest {
     @BeforeEach
     protected void setUp() {
         RestAssured.port = port;
+        memberRepository.save(mainMember);
+        when(oauth2ServiceConnector.getEmailFromOauth2(any(), any()))
+                .thenReturn(mainMember.getEmail());
+    }
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.delete(mainMember);
     }
 }
