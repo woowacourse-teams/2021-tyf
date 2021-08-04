49df6733654555c4fb08b25984a8d609120b8b49
dwl5 <ssop6403@gmail.com>
2021-08-05 05:11:59 +0900

    feat: loging 설정 추가

--- server/src/main/java/com/example/tyfserver/common/config/GlobalExceptionHandler.java
+++ server/src/main/java/com/example/tyfserver/common/config/GlobalExceptionHandler.java
@@ -3,23 +3,26 @@ package com.example.tyfserver.common.config;
 import com.example.tyfserver.common.dto.ErrorResponse;
 import com.example.tyfserver.common.exception.BaseException;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handle(BaseException exception) {
-        log.info("=== EXCEPTION ===", exception);
+        logger.info("=== EXCEPTION ===", exception);
         return ResponseEntity.badRequest().body(exception.toResponse());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleNotDefinedException(Exception exception) {
-        log.error("=== UNDEFINED EXCEPTION ===", exception);
+        logger.error("=== UNDEFINED EXCEPTION ===", exception);
         return ResponseEntity.badRequest().body(new ErrorResponse("error-000", "예상하지 못한 에러가 발생했습니다."));
     }
 }
