269a1110fdd8bad2bfac2686bfd080b38a65c12f
dwl5 <ssop6403@gmail.com>
2021-08-10 14:05:13 +0900

    feat: admin 패키지 추가

---
+++ server/src/main/java/com/example/tyfserver/admin/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.example.tyfserver.admin.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminService {
+}
