b0c3a02b8bdd665d97d958bfddbb2095a9388d7c
rok93 <goodboy302@naver.com>
2021-08-11 19:34:26 +0900

    refactor: MemberResponse에 bankRegistered 필드 추가

--- server/src/main/java/com/example/tyfserver/member/dto/MemberResponse.java
+++ server/src/main/java/com/example/tyfserver/member/dto/MemberResponse.java
@@ -1,5 +1,6 @@
 package com.example.tyfserver.member.dto;
 
+import com.example.tyfserver.member.domain.AccountStatus;
 import com.example.tyfserver.member.domain.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -14,17 +15,23 @@ public class MemberResponse {
     private String pageName;
     private String bio;
     private String profileImage;
+    private boolean bankRegistered;
 
     public MemberResponse(Member member) {
         this(member.getEmail(), member.getNickname(), member.getPageName(),
-                member.getBio(), member.getProfileImage());
+                member.getBio(), member.getProfileImage(), isBankRegistered(member));
     }
 
-    public MemberResponse(String email, String nickname, String pageName, String bio, String profileImage) {
+    private static boolean isBankRegistered(Member member) {
+        return member.getAccountStatus() == AccountStatus.REGISTERED;
+    }
+
+    public MemberResponse(String email, String nickname, String pageName, String bio, String profileImage, boolean bankRegistered) {
         this.email = email;
         this.nickname = nickname;
         this.pageName = pageName;
         this.bio = bio;
         this.profileImage = profileImage;
+        this.bankRegistered = bankRegistered;
     }
 }
--- server/src/test/java/com/example/tyfserver/member/MemberAcceptanceTest.java
+++ server/src/test/java/com/example/tyfserver/member/MemberAcceptanceTest.java
@@ -186,7 +186,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
         assertThat(memberResponse).usingRecursiveComparison()
                 .isEqualTo(new MemberResponse("email@email.com", "nickname", "pagename",
-                        "제 페이지에 와주셔서 감사합니다!", null)
+                        "제 페이지에 와주셔서 감사합니다!", null, false)
                 );
     }
 
@@ -206,7 +206,7 @@ public class MemberAcceptanceTest extends AcceptanceTest {
 
         assertThat(memberResponse).usingRecursiveComparison()
                 .isEqualTo(new MemberResponse("email@email.com", "nickname", "pagename",
-                        "제 페이지에 와주셔서 감사합니다!", null)
+                        "제 페이지에 와주셔서 감사합니다!", null, false)
                 );
     }
 
