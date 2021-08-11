8af71e55bef30f1598cddefe3b3be4b9d529de65
dwl5 <ssop6403@gmail.com>
2021-08-11 16:29:19 +0900

    refactor: 충돌 해결

--- server/src/main/java/com/example/tyfserver/admin/controller/AdminController.java
+++ server/src/main/java/com/example/tyfserver/admin/controller/AdminController.java
@@ -1,20 +1,13 @@
 package com.example.tyfserver.admin.controller;
 
 import com.example.tyfserver.admin.dto.AccountRejectRequest;
-import com.example.tyfserver.admin.dto.RequestingAccountResponse;
-import com.example.tyfserver.admin.service.AdminService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import com.example.tyfserver.admin.dto.AdminLoginRequest;
+import com.example.tyfserver.admin.dto.RequestingAccountResponse;
 import com.example.tyfserver.admin.service.AdminService;
 import com.example.tyfserver.auth.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,7 +35,6 @@ public class AdminController {
     public ResponseEntity<List<RequestingAccountResponse>> requestingAccounts() {
         return ResponseEntity.ok(adminService.findRequestingAccounts());
     }
-    private final AdminService adminService;
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody AdminLoginRequest adminLoginRequest) {
