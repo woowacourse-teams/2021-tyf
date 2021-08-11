cf611b2503004c82672cbd7b5200136d27910816
dwl5 <ssop6403@gmail.com>
2021-08-11 12:46:05 +0900

    refactor: member-010에서 009 으로 변경

--- server/src/main/java/com/example/tyfserver/member/exception/AccountRegisterValidationRequestException.java
+++ server/src/main/java/com/example/tyfserver/member/exception/AccountRegisterValidationRequestException.java
@@ -3,7 +3,7 @@ package com.example.tyfserver.member.exception;
 import com.example.tyfserver.common.exception.BaseException;
 
 public class AccountRegisterValidationRequestException extends BaseException {
-    public static final String ERROR_CODE = "member-010";
+    public static final String ERROR_CODE = "member-009";
     private static final String MESSAGE = "요청된 계좌의 정보의 형식이 올바르지 않습니다.";
 
     public AccountRegisterValidationRequestException() {
