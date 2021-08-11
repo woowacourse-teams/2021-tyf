65116c53a9d8d2e9c4e3ad818fb6248cdffcf8a3
Chayan <hchayan196@gmail.com>
2021-08-11 19:27:54 +0900

    refactor: 환불신청목록 컴포넌트 제거

--- client/admin/src/App.tsx
+++ client/admin/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import { NavBar } from './components/NavBar/NavBar';
 import loginPage from './pages/loginPage';
-import { refundPage } from './pages/refundPage';
 import { settlementPage } from './pages/settlementPage';
 
 const App = () => {
@@ -10,7 +9,6 @@ const App = () => {
       <NavBar />
       <Switch>
         <Route path="/" component={loginPage} exact />
-        <Route path="/refund" component={refundPage} />
         <Route path="/settlement" component={settlementPage} />
       </Switch>
     </>
