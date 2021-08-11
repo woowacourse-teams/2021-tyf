a324d2598b5ec93366b01d19b8b8d5ba869dec53
dwl5 <ssop6403@gmail.com>
2021-08-11 12:47:42 +0900

    refactor: AdminContoller RequestingMapping 적용  /admin

--- server/src/main/java/com/example/tyfserver/admin/controller/AdminController.java
+++ server/src/main/java/com/example/tyfserver/admin/controller/AdminController.java
@@ -6,33 +6,31 @@ import com.example.tyfserver.admin.service.AdminService;
 import com.example.tyfserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/admin/approve/{member_id}/account")
+    @PostMapping("/approve/{member_id}/account")
     public ResponseEntity<Void> approveAccount(@PathVariable("member_id") Long member_id) {
         adminService.approveAccount(member_id);;
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/admin/cancel/{member_id}/account")
+    @PostMapping("/cancel/{member_id}/account")
     public ResponseEntity<Void> cancelAccount(@PathVariable("member_id") Long member_id,
                                               AccountCancelRequest accountCancelRequest) {
         adminService.cancelAccount(member_id, accountCancelRequest);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/admin/list/account")
+    @GetMapping("/list/account")
     public ResponseEntity<List<RequestingAccountResponse>> requestingAccounts() {
         return ResponseEntity.ok(adminService.findRequestingAccounts());
     }
