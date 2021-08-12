280ad3aad7f454f60ebc1c285270a5907baa8a2d
dwl5 <ssop6403@gmail.com>
2021-08-12 12:41:37 +0900

    feat: html 및 임베디드 img 구현

--- server/src/main/java/com/example/tyfserver/common/util/SmtpMailConnector.java
+++ server/src/main/java/com/example/tyfserver/common/util/SmtpMailConnector.java
@@ -3,7 +3,8 @@ package com.example.tyfserver.common.util;
 import com.example.tyfserver.common.exception.SendingMailFailedException;
 import com.example.tyfserver.payment.domain.Payment;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,8 @@ import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -26,56 +29,42 @@ public class SmtpMailConnector {
     public void sendVerificationCode(String mailAddress, String verificationCode) {
         Context context = new Context();
         context.setVariable("code", verificationCode);
-        String message = templateEngine.process( "verification-code.html", context);
-
-        MimeMessage mail = javaMailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper;
-
-        try {
-            mimeMessageHelper = new MimeMessageHelper(mail, false, "UTF-8");
-            mimeMessageHelper.setTo(mailAddress);
-            mimeMessageHelper.setSubject(PREFIX_SUBJECT + "환불 인증번호");
-            mimeMessageHelper.setText(message, true);
-            javaMailSender.send(mail);
-        } catch (MessagingException e) {
-            throw new SendingMailFailedException();
-        }
+        String message = templateEngine.process("mail-verification-code.html", context);
+        sendMail("환불 인증번호", message, mailAddress);
     }
 
     public void sendExchangeApprove(String mailAddress) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(mailAddress);
-        message.setSubject(PREFIX_SUBJECT + "정산 승인 완료");
-        message.setText("정산이 완료되었습니다.");
-
-        javaMailSender.send(message);
+        Context context = new Context();
+        context.setVariable("head", "정산 승인 완료");
+        context.setVariable("body", "정산이 완료되었습니다.");
+        String message = templateEngine.process("mail-basic.html", context);
+        sendMail("정산 승인 완료", message, mailAddress);
     }
 
     public void sendExchangeReject(String mailAddress, String rejectReason) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(mailAddress);
-        message.setSubject(PREFIX_SUBJECT + "정산 승인 실패");
-        message.setText("정산이 거절되었습니다. \n " +
+        Context context = new Context();
+        context.setVariable("head", "정산 승인 반려");
+        context.setVariable("body", "정산이 반려되었습니다. \n " +
                 "거절사유 : " + rejectReason);
-        javaMailSender.send(message);
+        String message = templateEngine.process("mail-basic.html", context);
+        sendMail("정산 승인 반려", message, mailAddress);
     }
 
     public void sendAccountApprove(String mailAddress) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(mailAddress);
-        message.setSubject(PREFIX_SUBJECT + "정산 계좌 승인 완료");
-        message.setText("정산계좌 신청이 승인되었습니다.");
-
-        javaMailSender.send(message);
+        Context context = new Context();
+        context.setVariable("head", "정산 계좌 승인 완료");
+        context.setVariable("body", "정산계좌 신청이 승인되었습니다.");
+        String message = templateEngine.process("mail-basic.html", context);
+        sendMail("정산 계좌 승인 완료", message, mailAddress);
     }
 
     public void sendAccountReject(String mailAddress, String rejectReason) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(mailAddress);
-        message.setSubject(PREFIX_SUBJECT + "정산 계좌 승인 반려");
-        message.setText("정산계좌 신청이 반려되었습니다. \n " +
+        Context context = new Context();
+        context.setVariable("head", "정산 계좌 승인 반려");
+        context.setVariable("body", "정산계좌 신청이 반려되었습니다. \n " +
                 "반려사유 : " + rejectReason);
-        javaMailSender.send(message);
+        String message = templateEngine.process("mail-basic.html", context);
+        sendMail("정산 계좌 승인 반려", message, mailAddress);
     }
 
     public void sendDonationComplete(Payment payment) {
@@ -85,19 +74,33 @@ public class SmtpMailConnector {
         context.setVariable("donation_amount", payment.getAmount());
         context.setVariable("date", payment.getCreatedAt());
 
-        String message = templateEngine.process("donation-complete.html", context);
+        String message = templateEngine.process("mail-donation-complete.html", context);
+        sendMail("후원 성공", message, payment.getEmail());
 
+    }
+
+    private void sendMail(String subject, String htmlText, String toEmail) {
         MimeMessage mail = javaMailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper;
-
         try {
-            mimeMessageHelper = new MimeMessageHelper(mail, false, "UTF-8");
-            mimeMessageHelper.setTo(payment.getEmail());
-            mimeMessageHelper.setSubject(PREFIX_SUBJECT + "후원 성공");
-            mimeMessageHelper.setText(message, true);
+            mimeMessageHelper = new MimeMessageHelper(mail, true, "UTF-8");
+            mimeMessageHelper.setTo(toEmail);
+            mimeMessageHelper.setSubject(PREFIX_SUBJECT + subject);
+            mimeMessageHelper.setText(htmlText, true);
+            mimeMessageHelper.addInline("tyf-logo", getLogo());
             javaMailSender.send(mail);
         } catch (MessagingException e) {
             throw new SendingMailFailedException();
         }
+    }
+
+    private FileSystemResource getLogo() {
+        File file = null;
+        try {
+            file = new ClassPathResource("static/logo.png").getFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new FileSystemResource(file);
     }
 }
