b86671426dc1250a9dc4f40d2e2f3c519c227505
hwano <jho2301@gmail.com>
2021-08-25 13:11:04 +0900

    토스페이먼츠 결제수단 추가 (#319)

--- client/main/src/pages/Donation/Payment/DonationPaymentPage.tsx
+++ client/main/src/pages/Donation/Payment/DonationPaymentPage.tsx
@@ -7,11 +7,13 @@ import {
   StyledSubTitle,
 } from './DonationPaymentPage.styles';
 import KakaoPay from '../../../assets/icons/kakao-pay.svg';
+import asdf from '../../../assets/icons/google.svg';
 import useDonation from '../../../service//donation/useDonation';
 import { useParams } from 'react-router-dom';
 import { ParamTypes } from '../../../App';
 import usePageRefreshGuardEffect from '../../../utils/usePageRefreshGuardEffect';
 import Transition from '../../../components/@atom/Transition/Transition.styles';
+import OutlineButton from '../../../components/@molecule/OutlineButton/OutlineButton.styles';
 
 const DonationPaymentPage = () => {
   const { creatorId } = useParams<ParamTypes>();
@@ -25,7 +27,8 @@ const DonationPaymentPage = () => {
       <Transition>
         <StyledSubTitle>결제수단을 선택해주세요!</StyledSubTitle>
         <PaymentButtonContainer>
-          <IconOutlineBarButton src={KakaoPay} onClick={donate}>
+          <OutlineButton onClick={() => donate('uplus')}>일반 결제</OutlineButton>
+          <IconOutlineBarButton src={KakaoPay} onClick={() => donate('kakaopay')}>
             카카오페이
           </IconOutlineBarButton>
         </PaymentButtonContainer>
