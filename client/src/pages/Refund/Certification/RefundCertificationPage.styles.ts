084797cef73292206348e7b223f80d8a8f9cb919
Chayan <hchayan196@gmail.com>
2021-09-02 16:48:11 +0900

    refactor: 불필요한 파일들 제거

--- client/src/pages/Refund/Certification/RefundCertificationPage.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import Template from '../../../components/@atom/Template/Template';
-import { SIZE } from '../../../constants/device';
-
-export const StyledTemplate = styled(Template)`
-  display: flex;
-  max-width: ${SIZE.DESKTOP_LARGE}px;
-`;
