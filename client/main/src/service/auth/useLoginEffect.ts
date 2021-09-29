0a9b08f034eef47cbbc9546508c3b3d77818d0c6
Chayan <hchayan196@gmail.com>
2021-09-29 12:59:03 +0900

    feat(client): 비회원 로그인시 회원가입, 회원 회원가입시 로그인 플로우 추가 (#372)
    * feat: 비회원 로그인시 회원가입, 회원 회원가입시 로그인 플로우 추가
    
    * refactor: useRegisterPageName 함수형 컴포넌트 정상적으로 변경
    
    * feat: 가입된 사용자 아닙니다 문구 수정 및 자동 로그인 분기문 적용

--- client/main/src/service/auth/useLoginEffect.ts
+++ client/main/src/service/auth/useLoginEffect.ts
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
-import { AUTH_ERROR } from '../../constants/error';
 import { AUTH_CODE, OAUTH_ERROR, OAUTH_ERROR_DESC } from '../../constants/oauth';
 import { OAuthProvider } from '../../types';
 import { getQueryVariable } from '../../utils/queryString';
 import { requestLogin } from '../@request/auth';
 import useAccessToken from '../@shared/useAccessToken';
+import { AUTH_ERROR } from '../../constants/error';
+import { routeToOAuthPage } from './routeToOauthPage';
 
 const useLoginEffect = (oauthProvider?: OAuthProvider) => {
   const history = useHistory();
   const { storeAccessToken } = useAccessToken();
+
+  const handleRedirectionError = () => {
+    console.error(getQueryVariable(OAUTH_ERROR));
+    console.error(getQueryVariable(OAUTH_ERROR_DESC));
+
+    alert('로그인에 실패했습니다. 다시 시도해주세요.');
+  };
 
   const login = async (oauthProvider: OAuthProvider, authCode: string) => {
     try {
@@ -20,17 +28,13 @@ const useLoginEffect = (oauthProvider?: OAuthProvider) => {
       history.go(-3);
     } catch (error) {
       if (error.response.data.errorCode === AUTH_ERROR.NOT_USER) {
-        alert('회원가입 페이지로 이동합니다.');
-        history.push('/register');
+        alert('회원이 아닙니다. 회원가입 페이지로 이동합니다.');
+
+        routeToOAuthPage(oauthProvider, 'REGISTER');
+      } else {
+        alert('로그인에 실패했습니다.');
       }
     }
-  };
-
-  const handleRedirectionError = () => {
-    console.error(getQueryVariable(OAUTH_ERROR));
-    console.error(getQueryVariable(OAUTH_ERROR_DESC));
-
-    alert('로그인에 실패했습니다. 다시 시도해주세요.');
   };
 
   useEffect(() => {
