a0d7a6401add55a20404194a83d8df2ad7073185
dwl5 <ssop6403@gmail.com>
2021-10-01 15:11:48 +0900

    refactor(server): 테스트 시간을 단축한다 (DirtiesContext 사용하지 않도록) (#392)
    * refactor: DirtiesContext 사용하지 않도록 수정
    
    * refactor: Entity null 체크 Objects Notnull로 변경
    
    * refactor: DatabaseCleanUp 패키지 경로 변경
    
    * refactor: 스트림 변수 명 변경
    
    * refactor: database cleanup profile 제거

---
+++ server/src/test/java/com/example/tyfserver/DatabaseCleanup.java
@@ -0,0 +1,47 @@
+package com.example.tyfserver;
+
+import com.google.common.base.CaseFormat;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class DatabaseCleanup implements InitializingBean {
+
+    private final EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    public DatabaseCleanup(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        tableNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(entity -> Objects.nonNull(entity.getJavaType().getAnnotation(Entity.class)))
+                .map(entity -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, entity.getName()))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void cleanUp() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName +
+                    " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
