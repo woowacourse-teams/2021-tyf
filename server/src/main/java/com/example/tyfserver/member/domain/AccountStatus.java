83873cf50e33dcff7b54e5c7ca121200bc3cbb79
김주원 <kjw11077@naver.com>
2021-10-01 14:35:41 +0900

    refactor: Account 생성자 추가, MemberTest 리팩토링

--- server/src/main/java/com/example/tyfserver/member/domain/AccountStatus.java
+++ server/src/main/java/com/example/tyfserver/member/domain/AccountStatus.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum AccountStatus {
+    // todo 정산할 때 REGISTERED는 확인을 안하는데??
     UNREGISTERED("미등록"), REGISTERED("등록완료"), REQUESTING("등록요청"), REJECTED("등록반려");
 
     private final String information;
