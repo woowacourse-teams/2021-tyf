800e3bd67020890628018ca4044ad9db5dc4e237
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-10-02 14:12:13 +0900

    refactor: ; 오타 삭제

--- server/src/main/java/com/example/tyfserver/common/exception/DecryptException.java
+++ server/src/main/java/com/example/tyfserver/common/exception/DecryptException.java
@@ -2,7 +2,7 @@ package com.example.tyfserver.common.exception;
 
 public class DecryptException extends BaseException {
     public static final String ERROR_CODE = "common-002";
-    ;
+
     private static final String MESSAGE = "데이터를 복호화 하지 못했습니다.";
 
     public DecryptException() {
