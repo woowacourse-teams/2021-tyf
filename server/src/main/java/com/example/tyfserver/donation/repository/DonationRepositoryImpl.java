fa31951f82425db5a64a309bfc565c1d4d859155
김주원 <kjw11077@naver.com>
2021-10-06 04:45:50 +0900

    refactor: DonationRepositoryImpl 조건문 메서드 분리

--- server/src/main/java/com/example/tyfserver/donation/repository/DonationRepositoryImpl.java
+++ server/src/main/java/com/example/tyfserver/donation/repository/DonationRepositoryImpl.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.List;
 import java.util.Objects;
@@ -26,16 +27,14 @@ public class DonationRepositoryImpl implements DonationQueryRepository {
         Long result = queryFactory
                 .select(donation.point.sum())
                 .from(donation)
-                .where(waitingStatus(creatorId))
+                .where(
+                        donationOwner(creatorId),
+                        waitingForExchangeStatus()
+                )
                 .groupBy(donation.creator)
                 .fetchOne();
 
         return Objects.requireNonNullElse(result, 0L);
-    }
-
-    private BooleanExpression waitingStatus(Long creatorId) {
-        return donation.creator.id.eq(creatorId)
-                .and(donation.status.eq(DonationStatus.WAITING_FOR_EXCHANGE));
     }
 
     @Override
@@ -43,16 +42,14 @@ public class DonationRepositoryImpl implements DonationQueryRepository {
         Long result = queryFactory
                 .select(donation.point.sum())
                 .from(donation)
-                .where(exchangedStatus(creatorId))
+                .where(
+                        donationOwner(creatorId),
+                        exchangedStatus()
+                )
                 .groupBy(donation.creator)
                 .fetchOne();
 
         return Objects.requireNonNullElse(result, 0L);
-    }
-
-    private BooleanExpression exchangedStatus(Long creatorId) {
-        return donation.creator.id.eq(creatorId)
-                .and(donation.status.eq(DonationStatus.EXCHANGED));
     }
 
     @Override
@@ -61,11 +58,28 @@ public class DonationRepositoryImpl implements DonationQueryRepository {
                 .select(donation)
                 .from(donation)
                 .where(
-                        donation.creator.eq(creator),
-                        donation.status.eq(DonationStatus.WAITING_FOR_EXCHANGE),
-                        // 정산신청일자 다음달 1월1일 00:00 이전
-                        donation.createdAt.before(exchangeOn.plusMonths(1).atDay(1).atStartOfDay())
+                        donationOwner(creator.getId()),
+                        waitingForExchangeStatus(),
+                        beforeStartOfNextMonth(exchangeOn)
                 )
                 .fetch();
+    }
+
+    private BooleanExpression waitingForExchangeStatus() {
+        return donation.status.eq(DonationStatus.WAITING_FOR_EXCHANGE);
+    }
+
+    private BooleanExpression exchangedStatus() {
+        return donation.status.eq(DonationStatus.EXCHANGED);
+    }
+
+    private BooleanExpression donationOwner(Long creatorId) {
+        return donation.creator.id.eq(creatorId);
+    }
+
+    private BooleanExpression beforeStartOfNextMonth(YearMonth exchangeOn) {
+        // 정산신청일자 다음달 1일 00:00 이전
+        LocalDateTime startOfNextMonth = exchangeOn.plusMonths(1).atDay(1).atStartOfDay();
+        return donation.createdAt.before(startOfNextMonth);
     }
 }
