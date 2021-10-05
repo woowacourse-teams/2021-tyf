abf292174e1e7082fb68e614b2efd03feeab9089
김주원 <kjw11077@naver.com>
2021-10-06 06:18:20 +0900

    refactor: polish code

--- server/src/test/java/com/example/tyfserver/donation/domain/DonationTest.java
+++ server/src/test/java/com/example/tyfserver/donation/domain/DonationTest.java
@@ -1,7 +1,5 @@
 package com.example.tyfserver.donation.domain;
 
-import com.example.tyfserver.donation.dto.DonationMessageRequest;
-
 public class DonationTest {
 
     private static final String MESSAGE = "응원합니다";
@@ -12,13 +10,5 @@ public class DonationTest {
 
     public static Message testMessage(boolean secret) {
         return new Message("test", MESSAGE, secret);
-    }
-
-    public static DonationMessageRequest testMessageRequest() {
-        return new DonationMessageRequest(MESSAGE, false);
-    }
-
-    public static DonationMessageRequest testSecretMessageRequest() {
-        return new DonationMessageRequest(MESSAGE, true);
     }
 }
