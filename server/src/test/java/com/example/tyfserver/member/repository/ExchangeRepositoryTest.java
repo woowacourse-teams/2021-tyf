fcbd6c70a5c5e9a8bb080772c78f0b8305b2e30b
김주원 <kjw11077@naver.com>
2021-10-06 04:04:51 +0900

    refactor: 주석 제거

--- server/src/test/java/com/example/tyfserver/member/repository/ExchangeRepositoryTest.java
+++ server/src/test/java/com/example/tyfserver/member/repository/ExchangeRepositoryTest.java
@@ -77,29 +77,4 @@ class ExchangeRepositoryTest {
         assertThat(exchange.getMember()).isEqualTo(member);
         assertThat(exchange.getExchangeAmount()).isEqualTo(10000);
     }
-
-    //    @Test
-//    @DisplayName("페이지 네임으로 정산존재 확인")
-//    public void existsByPageName() {
-//        boolean result = exchangeRepository.existsByPageName(exchange.getMember().getPageName());
-//        assertThat(result).isTrue();
-//    }
-
-//    @Test
-//    @DisplayName("페이지 네임으로 정산존재 확인 - 못찾은 경우")
-//    public void existsByPageNameNotFound() {
-//        boolean result = exchangeRepository.existsByPageName("nope");
-//        assertThat(result).isFalse();
-//    }
-
-//    @Test
-//    @DisplayName("페이지 네임으로 정산 삭제")
-//    public void deleteByPageName() {
-//        boolean result = exchangeRepository.existsByPageName(exchange.getMember().getPageName());
-//        assertThat(result).isTrue();
-//
-//        exchangeRepository.deleteByPageName(exchange.getMember().getPageName());
-//        result = exchangeRepository.existsByPageName(exchange.getMember().getPageName());
-//        assertThat(result).isFalse();
-//    }
 }
