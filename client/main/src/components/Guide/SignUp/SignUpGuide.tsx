168afaaa7b60ce66048275daf0c16c4a4a31122d
jho2301 <jho2301@gmail.com>
2021-10-14 19:36:05 +0900

    feat: 회원가입 가이드 작성

--- client/main/src/components/Guide/SignUp/SignUpGuide.tsx
+++ client/main/src/components/Guide/SignUp/SignUpGuide.tsx
@@ -13,63 +13,71 @@ const SignUpGuide = () => {
     <StyledCreatorGuideContent>
       <StyledTitle>회원가입 방법</StyledTitle>
       <StyledContentContainer>
-        <StyledSubTitle>1. 계좌 인증하기</StyledSubTitle>
         <StyledContents>
           <StyledImage>
             <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/creator/account_1.png"
-              alt="정산 관리 버튼 선택 이미지"
+              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/1.png"
+              alt="회원가입 버튼"
             />
           </StyledImage>
           <StyledContent>
-            <p>1. 우측 메뉴바에서 [정산 관리] 를 선택하세요.</p>
+            <p>1. 우측 상단의 [회원가입]을 선택하세요.</p>
           </StyledContent>
         </StyledContents>
         <StyledContents>
           <StyledImage>
             <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/creator/account_2.png"
-              alt="정산 계좌 등록 이미지"
+              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/2.png"
+              alt="회원가입에 이용할 서비스 목록"
             />
           </StyledImage>
           <StyledContent>
-            <p>2. 본인 명의 계좌 정보를 입력해주세요.</p>
-            <p>3. 작성한 계좌 정보를 제출하세요.</p>
+            <p>2. 회원가입에 이용할 서비스를 선택합니다.</p>
           </StyledContent>
         </StyledContents>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/creator/account_3.png"
-              alt="등록 요청 확인 이미지"
-            />
+            <img src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/3.png" alt="약관 목록" />
           </StyledImage>
           <StyledContent>
             <p>
-              4. 제출된 요청이 승인될때까지 기다려주세요.
+              3. 약관을 확인한 후 동의를 해주신 다음,
               <br />
-              &nbsp;&nbsp;[정산 관리] 페이지에서 승인 여부를 확인할 수 있습니다.
+              &nbsp;&nbsp;[ 계속하기 ]를 눌러주세요.
             </p>
           </StyledContent>
         </StyledContents>
-      </StyledContentContainer>
-
-      <StyledContentContainer>
-        <StyledSubTitle>2. 정산 신청하기</StyledSubTitle>
         <StyledContents>
           <StyledImage>
             <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/creator/settlement_1.png"
-              alt="정산 신청하기 이미지"
+              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/4.png"
+              alt="내 페이지 주소 입력"
             />
           </StyledImage>
           <StyledContent>
-            <p>
-              1. 계좌 등록 승인 이후 [정산 관리] 페이지에서 [정산 신청하기] 를 요청하세요. <br />
-              &nbsp;&nbsp;정산 받을 수 있는 금액이 10000원 이상이여야만 합니다.
-            </p>
-
-            <p>2. 제출된 요청이 승인될때까지 기다려주세요.</p>
+            <p>4. 도네이션 받을 주소를 적어주세요.</p>
+          </StyledContent>
+        </StyledContents>
+        <StyledContents>
+          <StyledImage>
+            <img
+              src="https://de56jrhz7aye2.cloudfront.net/guides/guides/sign-up/5.png"
+              alt="내 닉네임 입력"
+            />
+          </StyledImage>
+          <StyledContent>
+            <p>5. 사용할 닉네임을 입력해주세요.</p>
+          </StyledContent>
+        </StyledContents>
+        <StyledContents>
+          <StyledImage>
+            <img
+              src="https://de56jrhz7aye2.cloudfront.net/guides/guides/sign-up/6.png"
+              alt="회원가입 완료"
+            />
+          </StyledImage>
+          <StyledContent>
+            <p>6. 회원가입이 완료되었습니다!</p>
           </StyledContent>
         </StyledContents>
       </StyledContentContainer>
