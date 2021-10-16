f522d32ceee143349a13238ef2fe7db8f2724297
jho2301 <jho2301@gmail.com>
2021-10-16 15:18:36 +0900

    feat: 회원가입 완료페이지, 계좌신청페이지 링크 버튼 추가

--- client/main/src/pages/Register/Success/RegisterSuccessPage.tsx
+++ client/main/src/pages/Register/Success/RegisterSuccessPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Anchor from '../../../components/@atom/Anchor/Anchor';
+import SubTitle from '../../../components/@atom/SubTitle/SubTitle.styles';
 import Transition from '../../../components/@atom/Transition/Transition.styles';
 import useRegister from '../../../service/auth/useRegister';
 import usePageRefreshGuardEffect from '../../../utils/usePageRefreshGuardEffect';
@@ -11,6 +12,8 @@ import {
   RegisterSuccessTitle,
   SuccessButtonContainer,
   StyledTemplate,
+  StyledSubTitle,
+  SettlementOutlineButton,
 } from './RegisterSuccessPage.styles';
 
 const RegisterSuccessPage = () => {
@@ -19,6 +22,10 @@ const RegisterSuccessPage = () => {
 
   const routeToCreatorPage = () => {
     history.push(`/creator/${user.pageName}`);
+  };
+
+  const routeToSettlementPage = () => {
+    history.push(`/creator/${user.pageName}/settlement`);
   };
 
   usePageRefreshGuardEffect(REGISTER_PAGE_KEY, false, '/register');
@@ -40,6 +47,10 @@ const RegisterSuccessPage = () => {
           <MyPageOutlineButton onClick={routeToCreatorPage}>
             🏠 내 페이지로 놀러가기
           </MyPageOutlineButton>
+          <StyledSubTitle>도네이션을 받고 싶다면?</StyledSubTitle>
+          <SettlementOutlineButton onClick={routeToSettlementPage}>
+            계좌 등록하기
+          </SettlementOutlineButton>
           <Anchor to="/">홈으로</Anchor>
         </SuccessButtonContainer>
       </Transition>
