404ea3469c5958b01cf1f804982e65ec701ec9e5
Chayan <hchayan196@gmail.com>
2021-12-12 15:20:59 +0900

    feat(client): 서버 이전에 따른 cloudfront 주소 변경

--- client/main/src/components/Guide/SignUp/SignUpGuide.tsx
+++ client/main/src/components/Guide/SignUp/SignUpGuide.tsx
@@ -1,3 +1,4 @@
+import { S3_URL } from '../../../constants/s3';
 import {
   StyledContentContainer,
   StyledCreatorGuideContent,
@@ -15,10 +16,7 @@ const SignUpGuide = () => {
       <StyledContentContainer>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/sign-up-1.png"
-              alt="회원가입 버튼"
-            />
+            <img src={`${S3_URL}/guides/sign-up/sign-up-1.png`} alt="회원가입 버튼" />
           </StyledImage>
           <StyledContent>
             <p>1. 우측 상단의 [회원가입]을 선택하세요.</p>
@@ -27,7 +25,7 @@ const SignUpGuide = () => {
         <StyledContents>
           <StyledImage>
             <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/sign-up-2.png"
+              src={`${S3_URL}/guides/sign-up/sign-up-2.png`}
               alt="회원가입에 이용할 서비스 목록"
             />
           </StyledImage>
@@ -37,10 +35,7 @@ const SignUpGuide = () => {
         </StyledContents>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/sign-up-3.png"
-              alt="약관 목록"
-            />
+            <img src={`${S3_URL}/guides/sign-up/sign-up-3.png`} alt="약관 목록" />
           </StyledImage>
           <StyledContent>
             <p>
@@ -52,10 +47,7 @@ const SignUpGuide = () => {
         </StyledContents>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/sign-up/sign-up-4.png"
-              alt="내 페이지 주소 입력"
-            />
+            <img src={`${S3_URL}/guides/sign-up/sign-up-4.png`} alt="내 페이지 주소 입력" />
           </StyledImage>
           <StyledContent>
             <p>4. 도네이션 받을 주소를 적어주세요.</p>
@@ -63,10 +55,7 @@ const SignUpGuide = () => {
         </StyledContents>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/guides/sign-up/sign-up-5.png"
-              alt="내 닉네임 입력"
-            />
+            <img src={`${S3_URL}/guides/guides/sign-up/sign-up-5.png`} alt="내 닉네임 입력" />
           </StyledImage>
           <StyledContent>
             <p>5. 사용할 닉네임을 입력해주세요.</p>
@@ -74,10 +63,7 @@ const SignUpGuide = () => {
         </StyledContents>
         <StyledContents>
           <StyledImage>
-            <img
-              src="https://de56jrhz7aye2.cloudfront.net/guides/guides/sign-up/sign-up-6.png"
-              alt="회원가입 완료"
-            />
+            <img src={`${S3_URL}/guides/guides/sign-up/sign-up-6.png`} alt="회원가입 완료" />
           </StyledImage>
           <StyledContent>
             <p>6. 회원가입이 완료되었습니다!</p>
