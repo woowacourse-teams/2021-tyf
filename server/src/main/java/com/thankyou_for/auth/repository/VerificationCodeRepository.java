339cdb4d59c7fa2dd0185e5144290d85c81a0e54
hchayanms <chayans999@gmail.com>
2022-08-07 15:18:47 +0900

    서버 파일 삭제

--- server/src/main/java/com/thankyou_for/auth/repository/VerificationCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thankyou_for.auth.repository;
-
-import com.thankyou_for.auth.domain.VerificationCode;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VerificationCodeRepository extends CrudRepository<VerificationCode, String> {
-}
--- server/src/main/resources/db/migration/prod/V3__TABLE_COLLATION_CHANGE.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `tyf`.`member`
-    CHANGE COLUMN `bio` `bio` VARCHAR(1500) CHARACTER SET 'utf8mb4' NULL DEFAULT NULL ;
-
-ALTER TABLE `tyf`.`donation`
-    CHANGE COLUMN `message` `message` VARCHAR(255) CHARACTER SET 'utf8mb4' NULL DEFAULT NULL ;
